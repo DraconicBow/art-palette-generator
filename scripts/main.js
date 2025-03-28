08dbacf4bb10f927006883450777c31b08dd80a1
DraconicBow <31867664+DraconicBow@users.noreply.github.com>
2025-03-28 08:00:25 +0500

    Update main.js

--- scripts/main.js
+++ scripts/main.js
@@ -37,7 +37,7 @@ async function generatePalette(img) {
     
     try {
         // Получаем 6 основных цветов
-        palette = colorThief.getPalette(img, 20);
+        palette = colorThief.getPalette(img, 30);
     } catch (error) {
         console.error('Error extracting palette:', error);
         return;
